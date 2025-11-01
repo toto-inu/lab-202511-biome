eb32feed66c1fba1be2fd83753c010a40a8247d0
toto-inu <oztodog@gmail.com>
2025-11-01 21:10:59 +0900

    update

---
+++ documents/101_lintルールのcategory.md
@@ -0,0 +1,15 @@
+a11y（アクセシビリティ）
+
+complexity（複雑さ）
+
+correctness（正しさ／誤り）
+
+performance（性能）
+
+security（セキュリティ）
+
+style（コーディングスタイル）
+
+suspicious（怪しいコード／バグになりやすい可能性）
+
+nursery（実験的ルール／開発中）
--- next_biome/app/page.tsx
+++ next_biome/app/page.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      {/* 別タブで開く */}
+      <a href="https://www.google.com" target="_blank" rel="noopener">
+        Googleリンク
+      </a>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
